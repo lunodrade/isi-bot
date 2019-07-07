7acd68151f1c064d2c13c6080c9de4bb5c1ab89c
Luciano Andrade <lunodrade@gmail.com>
2019-07-06 22:25:26 -0300

    Auto update and then restart phase 7

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ app.get('/oi', function (req, res) {
 
     //manda uma resposta pra quem enviou o GET (eg: acessar um site, requisitar dados de uma api)
     res.json({
-        message: 'oi! :)'
+        message: 'oi! testando com npx nodemon :)'
     });
 });
 
